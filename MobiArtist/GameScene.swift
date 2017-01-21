d8f5411dda9ada8ab0912d54a55991aac6aebbb8
valik711 <androvalik@gmail.com>
2017-01-21 10:36:47 +0300

    Helloworld app

--- MobiArtist/GameScene.swift
+++ MobiArtist/GameScene.swift
@@ -16,13 +16,6 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
